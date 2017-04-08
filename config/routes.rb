179e5b680aebe9cbb7e9d80b2432eb3016e61ca6
Ken Tran <loschorts@gmail.com>
2017-04-08 00:16:13 -0700

    fix user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  devise_for :users
 	namespace :api, defaults: {format: :json} do 
 	end
 	
--- db/migrate/20170408071134_add_user_info.rb
+++ db/migrate/20170408071134_add_user_info.rb
@@ -1,10 +1,8 @@
 class AddUserInfo < ActiveRecord::Migration[5.0]
   def change
-    create_table :users do |t|
-	  	t.string :first_name, null: false
-	  	t.string :last_name, null: false
-	  	t.string :language, null: false, default: "en"
-	  	t.string :about, null: false
-	  end
+  	add_column :users, :first_name, :string, null: false
+  	add_column :users, :last_name, :string, null: false
+  	add_column :users, :language, :string, null: false, default: "en"
+  	add_column :users, :about, :string, null: false
 	end
 end
