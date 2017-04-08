a236fefa536ba61417ec287c58e4938f0fdefa66
Ken Tran <loschorts@gmail.com>
2017-04-07 23:56:33 -0700

    rewrite migraiton

--- db/migrate/20170222044020_add_columns_to_users.rb
+++ db/migrate/20170222044020_add_columns_to_users.rb
@@ -1,9 +1,9 @@
 class AddColumnsToUsers < ActiveRecord::Migration[5.0]
   def change
-  	add_column :users, :first_name, :string, null: false
-  	add_column :users, :last_name, :string, null: false
-  	add_column :users, :language, :string, null: false, default: "en"
-    add_column :users, :string, :string, null: false
+  	t.string :first_name, null: false
+  	t.string :last_name, null: false
+  	t.string :language, null: false, default: "en"
+  	t.string :about, null: false
 
   	add_index :users, :first_name
   	add_index :users, :last_name
