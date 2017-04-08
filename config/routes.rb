9c0e37b77c814203b0ee7a63d13f196e159f3fde
Ken Tran <loschorts@gmail.com>
2017-04-07 23:50:59 -0700

    add preload file, delete erroneous migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  devise_for :installs
 	namespace :api, defaults: {format: :json} do 
-		end
+		devise_for :users
+	end
 	
   root to: "pages#index"
 end
